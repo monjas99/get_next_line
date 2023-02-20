e2c4adc3423a56f2b1d8c1718ab0db84e74b806f
monjas99 <118534492+monjas99@users.noreply.github.com>
2023-02-20 14:29:39 +0100

    Add files via upload

---
+++ get_next_line/gnl_santi/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 11:16:31 by dmonjas-          #+#    #+#             */
+/*   Updated: 2023/02/20 12:45:09 by dmonjas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_join(char *dest, char *s1, char *s2);
+
+#endif
---
+++ get_next_line/gnl_santi/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 13:03:22 by dmonjas-          #+#    #+#             */
+/*   Updated: 2023/02/20 12:45:55 by dmonjas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	main(void)
+{	
+	int		fd;
+	char	*line;
+	int		i;
+
+	i = 0;
+	fd = open("text", O_RDONLY);
+	if (fd <= 0)
+		return (0);
+	while (i < 9)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free (line);
+	}
+	return (0);
+}
